70e4a8e5215f8b4a5fd5acf82f0dfd67d7d43c63
MarsHallSec <47811548+MarsHallGans@users.noreply.github.com>
2020-03-21 04:57:26 -0700

    Add files via upload

---
+++ http.php
@@ -0,0 +1,20 @@
+<?php
+
+error_reporting(0);
+	
+	echo "\n\n";
+	echo "	Author   : MarsHall\n";
+	echo "	Team     : AnarchoXploit\n";
+	echo "	Tool     : Mass Tambahin Http\n\n";
+
+$list = file_get_contents("t.txt");
+
+$ex = explode("\n", $list);
+
+foreach($ex as $domain){
+
+$o = fopen("list2.txt", 'a');
+echo "http://".$domain."\n";
+fclose($o);
+
+}
